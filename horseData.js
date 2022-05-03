67ef11d04649c37fb5e9553700929e7836df5b65
Ryan Webster <ryan.h.webster@gmail.com>
2022-05-03 16:39:02 -0700

    horses db

---
+++ horseData.js
@@ -0,0 +1,24 @@
+const horses = [
+    { number: 1, name: 'Mo Donegal', bettor: null },
+    { number: 2, name: 'Happy Jack', bettor: null },
+    { number: 3, name: 'Epicenter', bettor: null },
+    { number: 4, name: 'Summer Is Tomorrow', bettor: null },
+    { number: 5, name: 'Smile Happy', bettor: null },
+    { number: 6, name: 'Messier', bettor: null },
+    { number: 7, name: 'Crown Pride', bettor: null },
+    { number: 8, name: 'Charge It', bettor: null },
+    { number: 9, name: 'Tiz The Bomb', bettor: null },
+    { number: 10, name: 'Zandon', bettor: null },
+    { number: 11, name: 'Pioneer of Medina', bettor: null },
+    { number: 12, name: 'Taiba', bettor: null },
+    { number: 13, name: 'Simplification', bettor: null },
+    { number: 14, name: 'Barber Road', bettor: null },
+    { number: 15, name: 'White Abarrio', bettor: null },
+    { number: 16, name: 'Cyberknife', bettor: null },
+    { number: 17, name: 'Classic Causeway', bettor: null },
+    { number: 18, name: 'Tawny Port', bettor: null },
+    { number: 19, name: 'Zozos', bettor: null },
+    { number: 20, name: 'Ethereal Road', bettor: null }
+]
+
+module.exports = horses
