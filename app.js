f249207caddc831020debee519ba4d6fb997a4af
Ryan Webster <ryan.h.webster@gmail.com>
2022-05-04 16:43:00 -0700

    middleware

--- app.js
+++ app.js
@@ -8,8 +8,29 @@ const morgan = require('morgan');
 
 const app = express();
 app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
 app.use(cookieParser());
 app.use(morgan('dev'));
+
+if (!isProduction) {
+    app.use(cors();)
+}
+
+app.use(helmet({
+    contentSecurityPolicy: false
+}))
+
+app.use(routes);
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use((_req, _res, next) => {
+    const err = new Error("The requested resource couldn't be found.");
+    err.title = "Resource Not Found";
+    err.errors = ["The requested resource couldn't be found."];
+    err.status = 404;
+    next(err);
+});
 
 const port = 8081;
 app.listen(port, () => console.log(`Listening on port ${port}...`));
