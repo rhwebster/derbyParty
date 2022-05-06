38d80d4845da2f363d6eddc1271461d0412399ca
Ryan Webster <ryan.h.webster@gmail.com>
2022-05-06 10:47:55 -0700

    horse list home page

--- frontend/src/components/HomePage.js
+++ frontend/src/components/HomePage.js
@@ -1,3 +1,31 @@
 import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { Modal } from '../../context/Modal';
+
+export default function Horses() {
+    const dispatch = useDispatch();
+    
+    const horses = useSelector(state => state.horses);
+    
+    const takenHorses = horses.filter(horse => horse.bettor);
+    const availableHorses = horses.filter(horse => !horse.bettor);
+
+    return (
+        <div id='home-page-background'>
+            <div className='taken-horse-list'>
+                <div className='placeholder'>
+                    {takenHorses && takenHorses.map(horse => {
+                        return (
+                            <Horse />
+                        )
+                    })}
+                    {availableHorses && availableHorses.map(horse => {
+                        return (
+                            <Horse />
+                        )
+                    })}
+                </div>
+            </div>
+        </div>
+    )
+}
